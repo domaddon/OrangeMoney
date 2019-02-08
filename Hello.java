bbd5061233af9702683204759cd2abc20d645851
SINGH Domeshwar TGI/OLS <domeshwar.singh@orange.com>
2019-02-08 15:04:22 +0530

    verify1

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, IHAHAHSSISISIWorld Orange Money hshhshshshshshhshshsjdjjd");
+      System.out.println("Hello, Chal gyya sccess");
    }
 }
