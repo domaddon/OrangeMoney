58d272d5f665e3cd8fc11707ebd916797bfac8a6
Domeshwar Singh <domeshwar.singh@orange.com>
2019-03-19 13:23:39 +0530

    branch title-change added holimessage

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, Chal gyya sccess");
+	  System.out.println("Happy Holi guys");
    }
 }
